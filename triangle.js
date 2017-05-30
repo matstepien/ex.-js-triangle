dc46dbdf48af0380d543b3a8a90978bb09ca3bcd
Mateusz <matstepien@outlook.com>
2017-05-30 16:02:39 +0200

    triangle

--- triangle.js
+++ triangle.js
@@ -1,5 +1,5 @@
-var a = parseInt('5'),
-	h = parseInt('4'),
+var a = 5,
+	h = 4,
 	triangleArea = a * h / 2;
 // alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
